d9adc4492123f7349382618333d362d5714da90d
trishachoudhary2006 <chmcs2026.trisha.choudhary@gmail.com>
2026-03-03 18:34:02 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 const api = axios.create({
-    baseURL: 'http://localhost:3000'
+  //  baseURL: 'http://localhost:3000'
+    baseURL : https://mern-doctorappointment.onrender.com
 })
 
 export default api
