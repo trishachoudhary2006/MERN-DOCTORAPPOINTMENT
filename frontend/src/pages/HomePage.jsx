276e83f895d731ac7e24265523bb3dfeefd88136
trishachoudhary2006 <chmcs2026.trisha.choudhary@gmail.com>
2026-03-14 21:22:10 +0530

    Update HomePage.jsx

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -66,8 +66,15 @@ const HomePage= () => {
           <input type="text" placeholder='Search by Patient or Doctor...' 
           className='input input-bordered' value={search} onChange={(e) => setSearch(e.target.value)} />
 
-          <input type="text" placeholder='Filter by Doctor...' 
-          className='input input-bordered' value={doctorName} onChange={(e) => setDoctorName(e.target.value)} />
+          {/* <input type="text" placeholder='Filter by Doctor...' 
+          className='input input-bordered' value={doctorName} onChange={(e) => setDoctorName(e.target.value)} /> */}
+          <select className="select select-bordered" value={doctorName} onChange={(e) => setDoctorName(e.target.value)}>
+            <option value="">All Doctors</option>
+            <option value="Dr Renu">Dr Renu</option>
+            <option value="Dr Sharma">Dr Sharma</option>
+            <option value="Dr Neeraja">Dr Neeraja</option>
+            <option value="Dr Ashish">Dr Ashish</option>
+           </select>
 
           <input type="date" className='input input-bordered' value={appointmentDate} onChange={(e) => setAppointmentDate(e.target.value)} />
 
